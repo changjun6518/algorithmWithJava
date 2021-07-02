df9704f0caa5750ce9cd3a1617080417dbbb502a
최창준 <aowert6518@gmail.com>
2021-07-02 09:29:16 +0900

    대소문자변환

--- src/src/inflearn/문자열/README.md
+++ src/src/inflearn/문자열/README.md
@@ -13,3 +13,12 @@ for(char x : str.toCharArray()){}
 ```
 문자열은 바로 함수, 문자는 character써서 해야함   
 iter쓰고싶으면 str을 문자배열로 바꿔줘야함
+
+[대소문자변환](대소문자변환.java)   
+처음 접근 => 정규표현식으로 소문자인지 대문자인지 확인   
+그리고 대소문자 변환해주는 함수 만들었음   
+그리고 static 스프링 만들어서 추가해서 결과 만들려고함
+```
+Character.isLowerCase(c)
+```
+Charector에 소문자 대문자 인지 확인하는 함수가 있다..
--- src/src/inflearn/문자열/대소문자변환.java
+++ src/src/inflearn/문자열/대소문자변환.java
@@ -1,2 +1,25 @@
-package inflearn.문자열;public class 대소문자변환 {
+package inflearn.문자열;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class 대소문자변환 {
+
+    static String result = "";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                result += Character.toUpperCase(c);
+            } else {
+                result += Character.toLowerCase(c);
+            }
+
+        }
+        System.out.printf(result);
+
+    }
+
 }
