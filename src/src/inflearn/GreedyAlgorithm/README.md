f4debdebddf30fbc8f38fc9f224e7313036acfe9
최창준 <aowert6518@gmail.com>
2021-08-05 14:30:18 +0900

    최대수스케쥴

--- src/src/inflearn/GreedyAlgorithm/README.md
+++ src/src/inflearn/GreedyAlgorithm/README.md
@@ -28,3 +28,29 @@ for (int i = 0; i < n; i++) {
 ```
 들어오는 거 하나하나를 따로 받아서 해석할 수 있다\
 char로 state를 오름차순 정리 할 수 있다
+
+[최대수스케쥴](최대수스케쥴.java)
+```
+ public static void greedy(ArrayList<Schedule> schedules) {
+    Collections.sort(schedules);
+    PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+    int j = 0;
+    for (int i = max; i >= 1; i--) {
+        for (; j < n; j++) {
+            if (schedules.get(j).limitDay < i) {
+                break;
+            }
+            pQ.offer(schedules.get(j).price);
+        }
+        if (!pQ.isEmpty()) {
+            sum += pQ.poll();
+        }
+    }
+}
+```
+PriorityQueue를 사용하는 문제\
+int j = 0;\
+for (int i = max; i >= 1; i--) {\
+for (; j < n; j++)\
+for문 돌릴 때 2중 포문인데 저렇게 할 수 있음
+
---
+++ src/src/inflearn/GreedyAlgorithm/최대수스케쥴.java
@@ -0,0 +1,75 @@
+package inflearn.GreedyAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class 최대수스케쥴 {
+    public static int sum = 0;
+    public static int n;
+    public static int max = Integer.MIN_VALUE;
+
+
+    private static class Schedule implements Comparable<Schedule> {
+        public int limitDay;
+        public int price;
+
+        public Schedule(int limitDay, int price) {
+            this.limitDay = limitDay;
+            this.price = price;
+        }
+
+        @Override
+        public int compareTo(Schedule o) {
+            return o.limitDay - this.limitDay;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        ArrayList<Schedule> schedules = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int price = sc.nextInt();
+            int limitDay = sc.nextInt();
+            schedules.add(new Schedule(limitDay, price));
+            if (limitDay > max) {
+                max = limitDay;
+            }
+        }
+        Collections.sort(schedules);
+        for (Schedule schedule : schedules) {
+            System.out.println(schedule.price);
+            System.out.println(schedule.limitDay);
+        }
+        greedy(schedules);
+        System.out.println(sum);
+
+    }
+
+    public static void greedy(ArrayList<Schedule> schedules) {
+        Collections.sort(schedules);
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+        int j = 0;
+        for (int i = max; i >= 1; i--) {
+            for (; j < n; j++) {
+                if (schedules.get(j).limitDay < i) {
+                    break;
+                }
+                pQ.offer(schedules.get(j).price);
+            }
+            if (!pQ.isEmpty()) {
+                sum += pQ.poll();
+            }
+        }
+
+    }
+}
+//6
+//50 2
+//20 1
+//40 2
+//60 3
+//30 3
+//30 1
